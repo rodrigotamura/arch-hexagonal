70cde9c788bd68d49366c74a5ef78db421d9beef
Rodrigo Tamura <rodrigo.tamura@ifood.com.br>
2022-11-05 16:26:17 -0300

    feat: added product entity and rules

---
+++ application/product_test.go
@@ -0,0 +1,65 @@
+package application_test
+
+import (
+	"testing"
+
+	"github.com/rodrigotamura/arch-hexagonal/application"
+	uuid "github.com/satori/go.uuid"
+	"github.com/stretchr/testify/require"
+)
+
+func TestProduct_Enable(t *testing.T) {
+	product := application.Product{}
+	product.Name = "Hello"
+	product.Status = application.DISABLED
+	product.Price = 10
+
+	err := product.Enable()
+
+	require.Nil(t, err) // verify must throw error (error == nil)
+
+	product.Price = 0
+	err = product.Enable()
+
+	require.Equal(t, "the price must be greater than 0 to enable the product", err.Error())
+}
+
+func TestProduct_Disabled(t *testing.T) {
+	product := application.Product{}
+	product.Name = "Hello"
+	product.Status = application.ENABLED
+	product.Price = 0
+
+	err := product.Disable()
+
+	require.Nil(t, err) // verify must throw error (error == nil)
+
+	product.Price = 10
+	err = product.Disable()
+
+	require.Equal(t, "the price must be equals to 0 in order to disable the product", err.Error())
+
+}
+
+func TestProduct_IsValid(t *testing.T) {
+	product := application.Product{}
+	product.ID = uuid.NewV4().String()
+	product.Name = "Hello"
+	product.Status = application.DISABLED
+	product.Price = 10
+
+	_, err := product.IsValid()
+	require.Nil(t, err)
+
+	product.Status = "INVALID"
+	_, err = product.IsValid()
+	require.Equal(t, "the status must be enabled or disabled", err.Error())
+
+	product.Status = application.ENABLED
+	_, err = product.IsValid()
+	require.Nil(t, err)
+
+	product.Price = -10
+	_, err = product.IsValid()
+	require.Equal(t, "the price must be greater or equal zero", err.Error())
+}
---
+++ go.mod
@@ -0,0 +1,13 @@
+module github.com/rodrigotamura/arch-hexagonal
+
+go 1.18
+
+require (
+	github.com/asaskevich/govalidator v0.0.0-20210307081110-f21760c49a8d // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/gofrs/uuid v4.3.1+incompatible // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/satori/go.uuid v1.2.0 // indirect
+	github.com/stretchr/testify v1.8.1 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
