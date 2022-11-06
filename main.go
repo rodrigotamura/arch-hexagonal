ee93d89cc32cd78fea099dad67d74bf9cb360203
Rodrigo Tamura <rodrigo.tamura@ifood.com.br>
2022-11-06 11:55:29 -0300

    feat: added adapter->cli

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func main() {
+	/*
+		db, _ := sql.Open("sqlite3", "sqlite.db")
+		productDbAdapter := db2.NewProductDb(db)
+		productService := application.NewProductservice(productDbAdapter)
+	*/
+}
